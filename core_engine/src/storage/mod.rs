6b45dcd44127171718f45f82b7dda245ca619244
Davi Bonetto <davi.bonetto100@gmail.com>
2026-01-21 13:51:51 -0300

    refactor(storage): update module exports for database integration

--- core_engine/src/storage/mod.rs
+++ core_engine/src/storage/mod.rs
@@ -1,7 +1,4 @@
-//! Storage Layer - L2 Probabilistic Cache
-//! 
-//! This module contains the Bloom Filter implementation for blocking
-//! requests to non-existent short codes before they hit the database.
+//! Storage Layer - L2 Probabilistic Cache + L4 Database
 
 pub mod bloom;
 pub use bloom::BloomStore;
